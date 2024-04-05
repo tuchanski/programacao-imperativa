be127efd554b4bf218e8c339752f0929fc4b01bc
Tuchanski <gui.rocha2004@gmail.com>
2024-04-05 18:06:04 -0300

    primeiro commit usando linux

---
+++ Teste no Linux/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* Recentemente mudei o sistema operacional do meu notebook para Ubuntu. 
+    Programa apenas para configurar a IDE e entender o processo com Linux p commitar.*/
+
+    printf("\nHello, Linux!\n");
+
+    return 0;
+}
